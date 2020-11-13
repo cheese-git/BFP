3df55c6f9cfcf93f651d849d9dd4cdf289b30379
cheese <cheese-h@qq.com>
2020-11-13 15:56:01 +0800

    koa-router

--- Phase 1/Week 1 +/resourse/index.js
+++ Phase 1/Week 1 +/resourse/index.js
@@ -1,8 +1,16 @@
-const koa = require('koa')
-const app = new koa()
+const Koa = require('koa')
+const Router = require('koa-router')
 
-app.use(async ctx => {
+const app = new Koa()
+const router = new Router
+
+router.get('/', async ctx => {
     ctx.body = "hello"
 })
 
+router.get('/api', async ctx => {
+    ctx.body = 'api'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 app.listen(3000)
