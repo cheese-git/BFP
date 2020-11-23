a35cc6785a943b964fad05788c6cb1ebaf340c36
cheese <cheese-h@qq.com>
2020-11-23 17:40:52 +0800

    captcha

--- imooc-backend-koa/app.js
+++ imooc-backend-koa/app.js
@@ -1,8 +1,17 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const svgCaptcha = require('svg-captcha')
 
 const app = new Koa()
 const router = new Router()
+
+router.get('/captcha', ctx => {
+    const { data, text } = svgCaptcha.create()
+    ctx.body = {
+        code: 200,
+        data
+    }
+})
 
 app.use(router.routes()).use(router.allowedMethods())
 
