f3a0a5c2cf95d7fde8519731fdea63421b6cdf23
cheese <cheese-h@qq.com>
2020-11-24 22:17:59 +0800

    fix duplicate log

--- imooc-frontend-vue2/src/api-fetch.ts
+++ imooc-frontend-vue2/src/api-fetch.ts
@@ -1,10 +1,13 @@
+let baseUrl: string
+
+if (process.env.NODE_ENV === 'production') {
+    baseUrl = "http://localhost:3000/"
+} else {
+    baseUrl = "http://localhost:3000/"
+    console.log('Using development api url.')
+}
+
 export default function apiFetch(path: string) {
-    let baseUrl
-    if (process.env.NODE_ENV === 'production') {
-        baseUrl = "http://localhost:3000/"
-    } else {
-        baseUrl = "http://localhost:3000/"
-        console.log('Using development api url.')
-    }
+
     return fetch(baseUrl + path).then(res => res.json())
 }
