a019212a025073990301d750202b756546213a66
cheese <cheese-h@qq.com>
2020-11-25 00:36:09 +0800

    pwd confirm

--- imooc-frontend-vue2/src/views/Register.vue
+++ imooc-frontend-vue2/src/views/Register.vue
@@ -19,6 +19,23 @@
         </b-form-group>
       </ValidationProvider>
       <pwd-input v-model="pwd" />
+      <ValidationProvider
+        name="确认密码"
+        rules="required|confirmPwd"
+        v-slot="{ errors }"
+      >
+        <b-form-group
+          label="确认密码"
+          :state="!errors[0]"
+          :invalid-feedback="errors[0]"
+        >
+          <b-form-input
+            type="password"
+            v-model="pwdConfirmed"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </ValidationProvider>
       <captcha-text-input v-model.trim="captchaText" />
 
       <b-button type="submit" variant="primary">立即注册</b-button>
@@ -31,15 +48,27 @@ import Vue from 'vue'
 import EmailInput from '@/components/EmailInput.vue'
 import PwdInput from '@/components/PwdInput.vue'
 import CaptchaTextInput from '@/components/CaptchaTextInput.vue'
+import { extend } from "vee-validate"
+
+
 
 export default Vue.extend({
   data() {
     return {
       username: '',
       pwd: '',
+      pwdConfirmed: '',
       captchaText: '',
       nickName: ''
     }
+  },
+  mounted() {
+    extend('confirmPwd', value => {
+      if (value === this.pwd) {
+        return true
+      }
+      return '两次密码不一致'
+    })
   },
   components: {
     EmailInput,
